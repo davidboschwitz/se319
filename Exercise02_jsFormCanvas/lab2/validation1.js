91cf5305c7ecff206e4af1acc4b585556fb079dc
Evan Warych <ewarych@iastate.edu>
2016-09-12 10:44:36 -0500

    Made the validation form look pretty and almost work, need to resize the image to append properly I think. Will work on that again soon.

--- Exercise02_jsFormCanvas/lab2/validation1.js
+++ Exercise02_jsFormCanvas/lab2/validation1.js
@@ -1,4 +1,43 @@
-function verify(){
+function verify(first, last, gender, state){
+    var correct = document.createElement("img");
+    correct.src = "./correct.png";
+    var wrong = document.createElement("img");
+    wrong.src = "./wrong.png";
 
+    if(first.value != ""){
+        var p = document.getElementById("first_p");
+        p.appendChild(correct);
+    }
+    else{
+        var p = document.getElementById("first_p");
+        p.appendChild(wrong);
+    }
+
+    if(last.value != ""){
+        var p = document.getElementById("last_p");
+        p.appendChild(correct);
+    }
+    else{
+        var p = document.getElementById("last_p");
+        p.appendChild(wrong);
+    }
+
+    if(gender.value != ""){
+       var p = document.getElementById("gender_p");
+       p.appendChild(correct);
+    }
+    else{
+       var p = document.getElementById("gender_p");
+       p.appendChild(wrong);
+    }
+
+    if(state.value != ""){
+       var p = document.getElementById("state_p");
+       p.appendChild(correct);
+    }
+    else{
+       var p = document.getElementById("state_p");
+       p.appendChild(wrong);
+    }
 }
 
